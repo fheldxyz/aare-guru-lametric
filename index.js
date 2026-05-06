f0e9e913574d88ec9da3ffebce707d98ddb1bc24
fheldxyz <fheldxyz@users.noreply.github.com>
2026-05-07 00:31:01 +0200

    Add lint and format tooling

--- index.js
+++ index.js
@@ -1,77 +1,72 @@
 "use strict";
 
 const express = require("express");
-const rp = require('request-promise-native');
+const rp = require("request-promise-native");
 const app = express();
 const port = process.env.PORT || 3000; //heroku and others use 'process.env.PORT'
 
-
-function processData(body,withgraph) {
-  
+function processData(body, withgraph) {
   var chartData = [];
   var aaredata = JSON.parse(body);
   var aaretemperaturestring = aaredata.aare.temperature.toString();
-  var loc  = aaredata.aare.location;
+  var loc = aaredata.aare.location;
   var temperature_text = aaredata.aare.temperature_text;
-  
-  aaretemperaturestring = loc + " " +  aaretemperaturestring + "°";
-  
-    
+
+  aaretemperaturestring = loc + " " + aaretemperaturestring + "°";
+
   var temp_past = aaredata.aarepast;
   var size_of_temp_past = temp_past.length;
-    
-  var end = Math.min(7*37, size_of_temp_past);
- 
-  for (let i = 0; i < Math.floor(end/7); i++) {
-    chartData[i]=Math.round(100*temp_past[7*i].temperature);
+
+  var end = Math.min(7 * 37, size_of_temp_past);
+
+  for (let i = 0; i < Math.floor(end / 7); i++) {
+    chartData[i] = Math.round(100 * temp_past[7 * i].temperature);
   }
-  
-  var min = Math.min(...chartData); 
+
+  var min = Math.min(...chartData);
   var max = Math.max(...chartData);
 
-  
   for (let i = 0; i < 37; i++) {
-    chartData[i]=Math.round((chartData[i]-min)*8/(max-min));
+    chartData[i] = Math.round(((chartData[i] - min) * 8) / (max - min));
   }
-  
-  
-  var result;
-  if (withgraph=="true"){
-	  result = {"frames":[
-		{"text":aaretemperaturestring,"icon":null},
-		{"text":temperature_text,"icon":2355},
-		{"index":1,"chartData":chartData} 
-	  ]};
-  }
-  else if (withgraph=="false"){
-	  result = {"frames":[
-		{"text":aaretemperaturestring,"icon":null},
-		{"text":temperature_text,"icon":2355}
-	  ]};
-  }
-   
-  return result;
 
+  var result;
+  if (withgraph == "true") {
+    result = {
+      frames: [
+        { text: aaretemperaturestring, icon: null },
+        { text: temperature_text, icon: 2355 },
+        { index: 1, chartData: chartData },
+      ],
+    };
+  } else if (withgraph == "false") {
+    result = {
+      frames: [
+        { text: aaretemperaturestring, icon: null },
+        { text: temperature_text, icon: 2355 },
+      ],
+    };
+  }
+
+  return result;
 }
 
-
-
 app.get("/", (req, res) => {
-
-    rp({
-      uri: "https://aareguru.existenz.ch/v2018/current?city="
-      + req.query.city
-      + "&"
-      + "app=xyz.fheld.lametric.aaretemperatur&version=1.1",
-      timeout: 3000
+  rp({
+    uri:
+      "https://aareguru.existenz.ch/v2018/current?city=" +
+      req.query.city +
+      "&" +
+      "app=xyz.fheld.lametric.aaretemperatur&version=1.1",
+    timeout: 3000,
+  })
+    .then((response) => processData(response, req.query.graph || "false"))
+    .then((response) => {
+      res.send(response);
     })
-    .then((response)=>processData(response,req.query.graph || "false" ))
-    .then(response => {
-        res.send(response)
-    })
-    .catch(error => {
-        res.send("error was caught")
-    })
-})
+    .catch(() => {
+      res.send("error was caught");
+    });
+});
 
 app.listen(port);
