123c4bc0ab160cba6ae2520c1b0a1dffe6329ae1
Florian Held <fheldxyz+github@gmail.com>
2026-06-01 23:14:11 +0200

    replace request-promise-native with fetch
    and update dependencies

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const express = require("express");
-const rp = require('request-promise-native');
 const app = express();
 const port = process.env.PORT || 3000; //heroku and others use 'process.env.PORT'
 
@@ -63,11 +62,12 @@ function processData(body,withgraph) {
 
 app.get("/", (req, res) => {
 
-    rp(
+    fetch(
       "https://aareguru.existenz.ch/v2018/current?city="
       + req.query.city 
       + "&" 
-      + "app=xyz.fheld.lametric.aaretemperatur&version=1.1")
+      + "app=xyz.fheld.lametric.aaretemperatur&version=1.1.1")
+    .then(r=>r.text())
     .then((response)=>processData(response,req.query.graph || "false" ))
     .then(response => {
         res.send(response)
